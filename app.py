832271eb168b64a0bdd20c047bfc8145329def79
Misha818 <misha818m@gmail.com>
2025-05-01 19:26:26 +0400

    redis Flask-Limiter Initialization Code

--- app.py
+++ app.py
@@ -66,14 +66,21 @@ def is_digit(value):
 
 
 # Initialize limiter with in-memory storage explicitly.
+# limiter = Limiter(
+#     app=app,
+#     key_func=get_remote_address,
+#     default_limits=["200 per day", "50 per hour"],
+#     storage_uri="memory://",  # explicitly using in-memory storage
+#     strategy="fixed-window"
+# )
+
 limiter = Limiter(
     app=app,
     key_func=get_remote_address,
     default_limits=["200 per day", "50 per hour"],
-    storage_uri="memory://",  # explicitly using in-memory storage
+    storage_uri="redis://localhost:6379",  # Use Redis storage
     strategy="fixed-window"
 )
-
 
 
 
