db70ed88b6b9ad1352f7a14139f25133159c69b5
Misha818 <misha818m@gmail.com>
2025-05-13 00:32:31 +0400

    j

--- app.py
+++ app.py
@@ -70,22 +70,22 @@ def is_digit(value):
 
 
 # Initialize limiter with in-memory storage explicitly.
-limiter = Limiter(
-    app=app,
-    key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"],
-    storage_uri="memory://",  # explicitly using in-memory storage
-    strategy="fixed-window"
-)
-
-# Initialize limiter with redis storage (for production)
 # limiter = Limiter(
 #     app=app,
 #     key_func=get_remote_address,
 #     default_limits=["200 per day", "50 per hour"],
-#     storage_uri="redis://localhost:6379/0",  # Use Redis storage
+#     storage_uri="memory://",  # explicitly using in-memory storage
 #     strategy="fixed-window"
 # )
+
+# Initialize limiter with redis storage (for production)
+limiter = Limiter(
+    app=app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="redis://localhost:6379/0",  # Use Redis storage
+    strategy="fixed-window"
+)
 
 
 
